f110e1611de72de2f9747594e81e3ace00e65d2d
Aayush Mittal <aayush102003@gmail.com>
2022-02-23 21:27:27 +0530

    chat room part 2

---
+++ 1000/2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    string s ="hello";
+    int count=0;
+    int j=0;
+    for (int i=0;i<str.length();i++)
+    {
+        if(str[i]==s[j])
+        {
+        count++;
+        j++;
+        if(count==5)
+        {
+            break;
+        }
+        }
+    }
+    if (count==5)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
