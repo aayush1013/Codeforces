de09ae3425f33ca04822c980b7ca42cf18538753
Aayush Mittal <aayush102003@gmail.com>
2022-03-31 16:58:29 +0530

    filling Diamonds

---
+++ 900/19.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+     int t;
+    cin>>t;
+    int area;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n<<endl;
+}
+    return 0;
+}
