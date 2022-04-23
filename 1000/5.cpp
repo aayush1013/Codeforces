1462e078c509c4db7a0c3a2c3b9d633f46b3634c
Aayush Mittal <aayush102003@gmail.com>
2022-04-23 23:30:20 +0530

    Expression

---
+++ 1000/5.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<list>
+typedef long long int ll;
+using namespace std;
+ 
+int main()
+{
+    ll a,b,c;
+    cin>>a>>b>>c;
+    ll w=a*(b+c);
+    ll x=(a+b)*c;
+    ll y=a*b+c;
+    ll z=a+b*c;
+    ll u=a*b*c;
+    ll t=a+b+c;
+    
+    vector<int> v = { w, x, y, z, u,t };
+    cout<<*max_element(v.begin(), v.end());
+	 
+return 0;
+}
