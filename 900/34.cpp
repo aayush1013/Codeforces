6acc1f9a4841f3d71da7ede5c6d039865bba990f
Aayush Mittal <aayush102003@gmail.com>
2022-05-01 23:18:43 +0530

    Anti Light's Cell Guessing

---
+++ 900/34.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <bits/stdc++.h>
+#define long long int ll;
+
+using namespace std;
+
+int main(){
+int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        if(n == 1 && m == 1){
+            cout << "0\n";
+        }
+        else if(min(n, m) == 1){
+            cout << "1\n";
+        }
+        else cout << "2\n";
+    }
+}
